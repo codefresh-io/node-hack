e56be97e670ee234af179b2cb323891c1d7d67d5
Oleg verhovsky <verchol@gmail.com>
2016-06-02 21:54:13 +0300

    add test

---
+++ tests/all.unit.spec.js
@@ -0,0 +1,19 @@
+describe('all unit tests', function(){
+  it('test1.1' , function(){
+
+  })
+  it('test1.1' , function(){
+
+  })
+  it('test1.1' , function(){
+
+  })
+
+  it('test1.1' , function(){
+
+  })
+  it('test1.2' , function(){
+
+  })
+
+})
